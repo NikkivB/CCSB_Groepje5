acfbd5a3c1b823dd3a341d8f620690d2c785765e
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-20 13:22:32 +0200

    Add project files.

---
+++ CCSB_Groepje5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CCSB_Groepje5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
