794ebcb93a09aafbcb11b67e75c0df9708aa4470
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-21 12:35:56 +0200

    0.1 gezamenlijk

--- CCSB_Groepje5/Startup.cs
+++ CCSB_Groepje5/Startup.cs
@@ -1,3 +1,5 @@
+using CCSB_Groepje5.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,7 +25,11 @@ namespace CCSB_Groepje5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
