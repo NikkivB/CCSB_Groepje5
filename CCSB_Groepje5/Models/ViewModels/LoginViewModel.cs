4e346bda7423660a095578b33c88d987472b658c
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-22 13:49:38 +0200

    login view model

---
+++ CCSB_Groepje5/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB_Groepje5.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        [EmailAddress(ErrorMessage ="Dit is geen geldig email adres")]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Password { get; set; }
+
+        [Display(Name = "Onthoud mij")]
+        public string RememberMe { get; set; }
+    }
+}
