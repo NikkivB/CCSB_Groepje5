b694b879fb7632cc6e147ceb2e5b0e525c37dae7
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-27 14:50:20 +0200

    edited AccountController.cs

--- CCSB_Groepje5/Controllers/AccountController.cs
+++ CCSB_Groepje5/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using CCSB_Groepje5.Models;
+using CCSB_Groepje5.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,16 +12,49 @@ namespace CCSB_Groepje5.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public AccountController(ApplicationDbContext db)
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<ApplicationUser> _roleManager;
+        public AccountController(ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationUser> roleManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _db = db;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult Register()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Register(RegisterViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
             return View();
         }
     }
--- CCSB_Groepje5/Startup.cs
+++ CCSB_Groepje5/Startup.cs
@@ -30,7 +30,7 @@ namespace CCSB_Groepje5
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
 
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
