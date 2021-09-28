e02db5081b7cb8ced72a09e3f394a849ab4ef87c
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-28 09:05:09 +0200

    added appointmentService/IAppointmentService

---
+++ CCSB_Groepje5/Services/IAppointmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB_Groepje5.Services
+{
+    public class IAppointmentService
+    {
+    }
+}
