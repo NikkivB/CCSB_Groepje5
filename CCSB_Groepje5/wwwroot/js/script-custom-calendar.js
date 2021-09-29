8400ad6f2cd1072fddd9b731ed7145ba1290d19d
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-29 14:19:29 +0200

    edited script-custom-calendar.js

--- CCSB_Groepje5/wwwroot/js/script-custom-calendar.js
+++ CCSB_Groepje5/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,27 @@
-﻿
+﻿$(document).ready(function () {
+    InitializeCalendar();
+});
+var calendar;
+function InitializeCalendar() {
+    try {
+        var calendarEl = document.getElementById('calendar');
+        if (calendarEl != null) {
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                locale: 'nl',
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                weekNumbers: true,
+                editable: false,
+            });
+            calendar.render();
+        }
+    }
+    catch (e) {
+        alert(e);
+    }
+}
