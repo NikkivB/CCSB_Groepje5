cd2a84a359dbe298046758dbfc53ca4dd348536b
[NikkivB] <0316641@student.rocvantwente.nl>
2021-09-30 09:35:41 +0200

    edited helper.cs

--- CCSB_Groepje5/Utility/Helper.cs
+++ CCSB_Groepje5/Utility/Helper.cs
@@ -10,6 +10,21 @@ namespace CCSB_Groepje5.Utility
     {
         public static readonly string Admin = "Beheerder";
         public static readonly string Klant = "Klant";
+
+        public static string AppointmentAdded = "Afspraak succesvol opgeslagen.";
+        public static string AppointmentConfirmed = "Afspraak bevestigd.";
+        public static string AppointmentUpdated = "Afspraak succesvol gewijzigd.";
+        public static string AppointmentDeleted = "Afspraak succesvol verwijderd.";
+
+        public static string AppointmentExists = "Afspraak bestaat al op gegeven datum en tijdstip.";
+        public static string AppointmentNotExists = "Afspraak bestaat niet.";
+        public static string AppointmentAddError = "Er ging iets mis. Afspraak niet toegevoegd.";
+        public static string AppointmentConfirmError = "Er ging iets mis. Afspraak niet bevestigd.";
+        public static string SomethingWentWrong = "Er ging iets mis. Probeer het opnieuw.";
+        public static string AppointmentUpdatError = "Er ging iets mis. Afspraak niet gewijzigd.";
+
+        public static int Succes_code = 1;
+        public static int Failure_code = 0;
         public static List<SelectListItem> GetTimeDropDown()
         {
             List<SelectListItem> duration = new List<SelectListItem>();
