41e51f108d44c5c67a595d0933aa2fb609261fed
Nikki <nikkivanbraam@hotmail.nl>
2021-10-07 09:33:38 +0200

    added vehicleType to RegisterViewModel

---
+++ CCSB_Groepje5/Models/ViewModels/RegisterVehicleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB_Groepje5.Models.ViewModels
+{
+    public class RegisterVehicleViewModel
+    {
+        [DisplayName("Klant")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        public string Customer { get; set; }
+
+        [DisplayName("Kentekenplaat")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        public string LicensePlate { get; set; }
+
+        [DisplayName("Soort voertuig")]
+        [Required(ErrorMessage = "{0} is een verplicht veld.")]
+        public string VehicleType { get; set; }
+    }
+}
