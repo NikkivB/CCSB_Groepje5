b36883c6e3e918bf77f21f2d99a1ebcde54978bb
Nikki <nikkivanbraam@hotmail.nl>
2021-11-15 12:21:55 +0100

    added model: Prices.cs

---
+++ CCSB_Groepje5/Models/Prices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    namespace CCSB_Groepje5.Models
+{
+    public class Prices
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string VehicleType { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime EditDate { get; set; }
+           
+    }
+}
